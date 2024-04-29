f6bba5e519d25af60349b95b8abee8cc01f61ce0
Ashley Grealish <ashley@grealish.co.uk>
2024-04-29 20:05:06 +0100

    Refactor of j1772 and hal classes

--- hal.py
+++ hal.py
@@ -1,13 +1,30 @@
 from gpiozero import PWMOutputDevice, DigitalOutputDevice, InputDevice, SPIDevice
 
+
 class ChargerHAL:
     def __init__(self):
         self.control_pilot = PWMOutputDevice(pin="GPIO12", frequency=1000)
-        self.control_pilot_read = InputDevice(pin="GPIO3")
-        self.energy_monitor = SPIDevice(channel=0, device=0)
-        self.relay = DigitalOutputDevice(pin="GPIO5", active_high=True, initial_value=False)
+
+        # Todo: Check this pull-up
+        self.control_pilot_read = InputDevice(pin="GPIO3", pull_up=True)
+
+        # Todo: Catch the warning for disabled SPI device
+        self.energy_monitor = SPIDevice(port=0, device=0)
+
+        self.relay = DigitalOutputDevice(
+            pin="GPIO5",
+            active_high=True,
+            initial_value=False
+        )
+
+    def initiate_cp_pin_high(self):
+        self.control_pilot.value = 1.0
+
+    def _set_cp_pwm(self, pwm_value: float) -> None:
+        self.control_pilot.value = pwm_value
 
     def set_current_limit(self, amps: int) -> None:
+        # Todo: Move this logic into j1772 class
         if amps < 6:
             pwm_value = 0.0
         elif amps > 80:
@@ -15,7 +32,7 @@ class ChargerHAL:
         else:
             pwm_value = (amps - 6) / 24 * 0.4 + 0.1
 
-        self.control_pilot.value = pwm_value
+        self._set_cp_pwm(pwm_value)
 
     def read_cp_voltage(self) -> float:
         return self.control_pilot_read.value * 3.3
--- j1772.py
+++ j1772.py
@@ -2,12 +2,16 @@ from enum import Enum
 import time
 from hal import ChargerHAL
 
+MAX_CURRENT = 6
+
+
 class CPState(Enum):
     VEHICLE_NOT_CONNECTED = "Vehicle not connected"
     VEHICLE_CONNECTED_NOT_READY = "Vehicle connected, not ready to charge"
     VEHICLE_CONNECTED_READY = "Vehicle connected, ready to charge"
     VEHICLE_CHARGING = "Vehicle charging"
     UNKNOWN = "Unknown state"
+
 
 class J1772Charger:
     def __init__(self, hal: ChargerHAL):
@@ -33,31 +37,35 @@ class J1772Charger:
             voltage = self.hal.read_voltage()
             current = self.hal.read_current()
             power = voltage * current
-            print(f"Charging status: {self.charging_current}A, {voltage}V, {current}A, {power}W")
+            print(
+                f"Charging status: {self.charging_current}A, {voltage}V, {current}A, {power}W")
         else:
             print("Not charging")
 
     def read_cp_state(self) -> CPState:
         cp_voltage = self.hal.read_cp_voltage()
 
-        if cp_voltage >= 0.0 and cp_voltage < 3.0:
+        if 0.0 <= cp_voltage < 3.0:
             return CPState.VEHICLE_NOT_CONNECTED
-        elif cp_voltage >= 3.0 and cp_voltage < 8.0:
+        elif 3.0 <= cp_voltage < 8.0:
             return CPState.VEHICLE_CONNECTED_NOT_READY
-        elif cp_voltage >= 8.0 and cp_voltage < 11.0:
+        elif 8.0 <= cp_voltage < 11.0:
             return CPState.VEHICLE_CONNECTED_READY
-        elif cp_voltage >= 11.0 and cp_voltage < 12.0:
+        elif 11.0 <= cp_voltage < 12.0:
             return CPState.VEHICLE_CHARGING
         else:
             return CPState.UNKNOWN
 
     def monitor_charging(self) -> None:
+        # Set a 12V signal initially
+        self.hal.initiate_cp_pin_high()
+
         while True:
             cp_state = self.read_cp_state()
             print("CP State:", cp_state.value)
 
             if cp_state == CPState.VEHICLE_CONNECTED_READY and not self.is_charging:
-                self.start_charging(30)  # Set charging current to 30A
+                self.start_charging(MAX_CURRENT)
             elif cp_state == CPState.VEHICLE_NOT_CONNECTED and self.is_charging:
                 self.stop_charging()
 
